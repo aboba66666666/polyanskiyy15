66efa138b2eea3afe742ec3beae6942d5ef5f024
сас <@DESKTOP-TBMAHQK>
2022-05-02 19:59:17 +0300

    Добавьте файлы проекта.

---
+++ polyanskiyy15/polyanskiyy15/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using polyanskiyy15.Models;
+using polyanskiyy15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace polyanskiyy15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+    
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
